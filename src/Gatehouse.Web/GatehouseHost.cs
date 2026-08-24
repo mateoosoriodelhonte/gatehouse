0b1eb4ec9b3caab9d8b27c55d1e074c980295dbf
Mateo Osorio Delhonte <259801526+mateoosoriodelhonte@users.noreply.github.com>
2026-08-24 11:59:25 -0600

    fix: load release static web assets

--- src/Gatehouse.Web/GatehouseHost.cs
+++ src/Gatehouse.Web/GatehouseHost.cs
@@ -6,6 +6,7 @@ using Gatehouse.Infrastructure.Persistence;
 using Gatehouse.Web.Ui;
 using Gatehouse.Web.Components;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -75,6 +76,10 @@ public static class GatehouseHost
         builder.Services.AddScoped<UiSessionState>();
         configureServices?.Invoke(builder.Services);
         var enableUi = builder.Configuration.GetValue("Gatehouse:EnableUi", true);
+        if (enableUi)
+        {
+            StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+        }
 
         var app = builder.Build();
         await MigrateDatabaseAsync(app.Services, cancellationToken);
